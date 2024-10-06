666989cbb9bc64aea2ea974108d7550dbcda3c95
Simone Arena <122620587+WasamiKirua@users.noreply.github.com>
2024-10-06 11:18:44 +0200

    Update entry_point.sh

--- entry_point.sh
+++ entry_point.sh
@@ -5,19 +5,6 @@
 # https://docs.openshift.com/container-platform/3.11/creating_images/guidelines.html#openshift-specific-guidelines
 #==============================================
 
-# added for tor
-# check if "ENABLE_TOR" is set to "true"
-if [ "$ENABLE_TOR" = "true" ]; then
-    echo "Enabling Tor"
-    # start tor
-    sudo service tor start
-    # start privoxy
-    sudo service privoxy start
-fi
-
-# check if ENABLE_PROTONVPN is set to "true"
-sh /opt/change_vpn.sh
-
 ### added for cronjob
 sudo service cron stop
 sudo service cron reload
